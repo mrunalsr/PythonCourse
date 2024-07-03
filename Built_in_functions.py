6109d2c95303856e896f18500b6c722d5f8e2a18
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2024-07-03 23:20:54 +0530

    Add files via upload

---
+++ Built_in_functions.py
@@ -0,0 +1,20 @@
+#Built in functions : print , len , max, min, type etc
+#print()
+print("Hello world")
+
+#len()
+length = "Welcome to my college"
+print(len(length))
+
+#max()
+list = [12,34,3,56,45]
+print(max(list))
+#min()
+print(min(list))
+
+#type
+print(type(list))
+
+def demo():
+    print("Hello")
+print(type(demo))
