41e4c55bb6297fed837fbad22f2a1acfbf0a85a4
Mrunali Satish Raksale <145702444+mrunalsr@users.noreply.github.com>
2024-07-03 23:36:32 +0530

    Add files via upload

---
+++ ifelif_statement.py
@@ -0,0 +1,38 @@
+"""age = int(input("Enter your age here : "))
+if age <12:
+    print("Please pay Rs 150")
+elif age <= 18:
+    print("Please pay Rs 250")
+else:
+    print("Please pay Rs 550")
+print("Thank you...\nVisit again")"""
+
+"""height = int(input("Enter your height please : "))
+
+if height>=3:
+    print("You are able to ride...")
+    age = int(input("Enter your age please : "))
+    if age <=18:
+        print("Please pay Rs 250 for rides")
+    else:
+        print("Please pay Rs 550 for rides")
+
+else:
+    print("You are not able to ride")
+print("Thank you ...."
+      "visit again")"""
+
+#EXERCISE
+num = int(input("Enter the number here : "))
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+elif num == 3:
+    print("Three")
+elif num == 4:
+    print("Four")
+else:
+    print("Invalid input")
+
+print("thank you...")
